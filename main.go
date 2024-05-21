c7eb5c611e517c23c1f059d5202adfa0f129be09
Naral <77269201+NaralC@users.noreply.github.com>
2024-05-21 11:07:42 +0100

    Variable & Type Declarations

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+func variablesAndTypes() {
+	// var name type = expression
+	// defaults to 0, "", false, null if unassigned
+	var integer int
+	var integer1, integer2 = 2, "string"
+
+	// name := expression
+	goIsAwesome, javaIsAwesome := true, false
+
+	fmt.Println(integer)
+	fmt.Println(integer1, integer2)
+	fmt.Println(goIsAwesome, javaIsAwesome)
+}
+
+func pointers()  {
+	// var x int = 1
+	// var p *int = &x
+	x := 1
+	p := &x
+
+	fmt.Println(p)
+	fmt.Println(*p)
+}
+
+func types() {
+	type farenheit int
+	type celsius int
+
+	var f farenheit = 32
+	var c celsius = 0
+
+	c = celsius((f - 32) * 5 / 9)
+
+	fmt.Println(f, c)
+}
+
 func main()  {
-	fmt.Println("Henlo World")
+	// variablesAndTypes()
+	// pointers()
+	types()
 }
